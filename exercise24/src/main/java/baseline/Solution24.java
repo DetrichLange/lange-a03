18de40aa788560c340bb3a67c4acc6da0d650728
unknown <detrich.lange@gmail.com>
2021-09-23 18:51:37 -0400

    first commit

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,40 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Detrich Lange
+ */
+
+package baseline;
+import java.util.Scanner;
+
+/*
+Create a program that compares two strings and determines if the two strings are anagrams. The program should prompt
+for both input strings and display the output as shown in the example that follows.
+    Implement the program using a function called isAnagram, which takes in two words as its arguments and returns true or false. Invoke this function from your main program.
+    Check that both words are the same length.
+ */
+
+public class Solution24 {
+    static final Scanner userInput = new Scanner(System.in);
+
+    void introduction(){
+        //Print the introduction/explanation of how the program works
+    }
+
+    String promptString(String whichString){
+        //Prompt user to enter a string, return the string entered
+    }
+
+    public static void main(String[] args){
+        //Create a new anagramSolver object for the solution
+        //Initialize two strings
+
+        //run introduction() to tell the user what to do
+
+        //Run promptString("first") and promptString("second") to collect both strings
+
+        //Make an anagramsolver object with both strings as arguments
+
+        //Run anagramSolver.solveAnagram to do the rest of the work (see AnagramSolver class)
+    }
+
+}
