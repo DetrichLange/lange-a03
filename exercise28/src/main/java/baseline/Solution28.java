193d0755af1d4f53a9c2ecfaccaf70633ed2f034
unknown <detrich.lange@gmail.com>
2021-09-28 15:14:45 -0400

    added pseudocode for 28

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,25 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Detrich Lange
+ */
+
+package baseline;
+import java.util.Scanner;
+
+/*
+Write a program that prompts the user for five numbers and computes the total of the numbers.
+    The prompting must use repetition, such as a counted loop, not three separate prompts.
+ */
+
+public class Solution28 {
+    static final Scanner userInput = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        //For i=0, i<5, i++:
+            //Declare int numberTotal = 0
+            //Prompt user for number
+            //Try to add inputted value to int, catch NFE
+        //Print output statement with numberTotal
+    }
+}
