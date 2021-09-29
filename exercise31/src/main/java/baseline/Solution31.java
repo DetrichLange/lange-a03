49022821061a34a8e1abed79cbecdbc9beb5b5cb
unknown <detrich.lange@gmail.com>
2021-09-29 17:46:31 -0400

    solution and test for 31

--- exercise31/src/main/java/baseline/Solution31.java
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -18,27 +18,61 @@ TargetHeartRate = (((220 − age) − restingHR) × intensity) + restingHR
  */
 
 public class Solution31 {
+    static final Scanner userInput = new Scanner(System.in);
+    static final String ERRORMESSAGE = "Please enter a number.";
 
-    public static void main(String[] args) {
+    //Set minIntensity to 55, maxIntensity to 95, incrementIntensity to 5
+    static int minIntensity = 55;
+    static int maxIntensity = 95;
+    static int incrementIntensity = 5;
+
+    private double promptValue(String whichNumber){
         //While true:
-            //Prompt user for age
-            //Try to return entered age as int, then break
+            //Prompt user for specified value
+            //Try to return entered line as int, then break
                 //Catch NFE, print error message and continue loop
+        while(true){
+            System.out.printf("Please enter %s:%n", whichNumber);
+            try{
+                return Double.parseDouble(userInput.nextLine());
+            }catch(NumberFormatException nfe){
+                System.out.println(ERRORMESSAGE);
+            }
+        }
+    }
 
-        //While true:
-            //Prompt user for resting heart rate
-            //Try to return entered heart rate as int, then break
-                //Catch NFE, print error message and continue loop
-
+    public StringBuilder buildHeartRateTable(double userAge, double userRestingHeartRate){
         //Create StringBuilder named outputTable
-        //Append header line
-        //Append divider line
+        StringBuilder outputTable = new StringBuilder();
 
-        //Set minIntensity to 55, maxIntensity to 95, incrementIntensity to 5
+        //Append header line
+        outputTable.append("Intensity   | Rate\n");
+
+        //Append divider line
+        outputTable.append("------------|--------\n");
 
         //Loop for i=minIntensity, i<=maxIntensity, i+=incrementIntensity
-            //Append intensity padded to 13 chars, divider, target heart rate bpm
+            //Append intensity padded to 13 chars, divider, target heart rate, bpm
+        for(double i=minIntensity; i<=maxIntensity; i+=incrementIntensity){
+            outputTable.append(String.format("%.0f%%", i));
+            outputTable.append(" ".repeat(Math.max(0, 13 - String.valueOf(i).length())));
+            outputTable.append(String.format("| %.0f bpm", (((220-userAge)-userRestingHeartRate)*(i/100)) + userRestingHeartRate));
+            outputTable.append("\n");
+        }
+
+        return outputTable;
+    }
+
+    public static void main(String[] args) {
+        Solution31 solutionApp = new Solution31();
+
+        //Prompt user for age and resting heart rate
+        double userAge = solutionApp.promptValue("your age");
+        double userRestingHeartRate = solutionApp.promptValue("your resting heart rate");
+
+        StringBuilder outputTable = solutionApp.buildHeartRateTable(userAge, userRestingHeartRate);
 
         //Print outputTable
+        System.out.print(outputTable);
     }
 }
