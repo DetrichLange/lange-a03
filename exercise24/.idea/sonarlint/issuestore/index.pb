054c549c1017495d0cb3b9d5007efcf3418c96ad
unknown <detrich.lange@gmail.com>
2021-09-29 16:31:50 -0400

    pseudocode for 30

---
+++ exercise24/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+V
+&src/main/java/baseline/Solution24.java,6\a\6a3518ac797c1fdae3479160a6036bf1929dcb92
+^
+.src/test/java/testbaseline/Solution24Test.java,0\9\091f1726dc01757643c4f66aed4cdb3cabc94f7b
---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,22 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Detrich Lange
+ */
+
+package baseline;
+
+/*
+Create a program that generates a multiplication table for the numbers 1 through 12 (inclusive).
+    Use a nested loop to complete this program.
+    Align each column within the table without using the tab character.
+ */
+
+public class Solution30 {
+
+    public static void main(String[] args) {
+        //For-loop i=0, i<=10, i++
+            //For-loop q=0, q<10, q++
+                //Printf "%4d", i*q
+            //Printf "%n"
+    }
+}
