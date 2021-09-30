cfd3dd28333145c99974587fd5482635132a5203
unknown <detrich.lange@gmail.com>
2021-09-30 18:23:09 -0400

    pseudocode for 33

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,37 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Detrich Lange
+ */
+
+package baseline;
+import java.util.Random;
+import java.util.Scanner;
+
+/*
+Create a Magic 8 Ball game that prompts for a question and then displays either "Yes," "No," "Maybe," or "Ask again
+later."
+    The value should be chosen randomly using a pseudo random number generator. Store the possible choices in a list
+    and select one at random.
+ */
+
+public class Solution33 {
+    Scanner userInput = new Scanner(System.in);
+    Random rng = new Random();
+
+    private void promptQuestion(){
+
+    }
+
+    public static void main(String[] args) {
+        Solution33 solutionApp = new Solution33();
+
+        //Create an array with the potential answers
+
+        //Call a method that prompts the user for a question and reads it, returns void
+
+        //Call a method using the array of answers as a parameter. The method selects a random answer from the array
+        //and returns it.
+
+        //Print the returned string.
+    }
+}
