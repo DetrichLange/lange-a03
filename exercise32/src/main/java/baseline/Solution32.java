2c1fe26b04422cb099a197c2d87e129b61a5ed28
unknown <detrich.lange@gmail.com>
2021-09-30 15:12:50 -0400

    solution and test for 32

--- exercise32/src/main/java/baseline/Solution32.java
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -4,6 +4,8 @@
  */
 
 package baseline;
+import java.util.Random;
+import java.util.Scanner;
 
 /*
 Write a Guess the Number game that has three levels of difficulty. The first level of difficulty would be a number
@@ -19,45 +21,155 @@ the correct number, the computer should present the number of guesses and ask if
  */
 
 public class Solution32 {
+    static final Scanner userInput = new Scanner(System.in);
+    static final String ERRORMESSAGE = "You must enter 1, 2, or 3.";
+    static final Random rng = new Random();
+
+    private int promptDifficulty(){
+        //in a loop:
+            //prompts player for difficulty level
+            //tries to parse that value as an int
+                //If NFE, prints error message (must enter an int 1, 2, or 3) and restart loop
+            //checks if the value is between 1 and 3 inclusive
+                //If it is, returns the entered difficulty level
+            //prints error message
+
+        int difficultyLevel;
+
+        while(true){
+            System.out.println("Enter the difficulty level (1, 2, or 3):");
+            try{
+                difficultyLevel = Integer.parseInt(userInput.nextLine());
+                if(difficultyLevel > 0 && difficultyLevel < 4){
+                    return difficultyLevel;
+                }
+                else{
+                    System.out.println(ERRORMESSAGE);
+                }
+            }catch(NumberFormatException nfe){
+                System.out.println(ERRORMESSAGE);
+            }
+        }
+    }
+
+    public int randomTarget(int difficultyLevel){
+        //if difficulty is 1, return random int between 1 and 10 inclusive
+        if(difficultyLevel == 1){
+            return rng.nextInt(11);
+        }
+        //if difficulty is 2, return random int between 1 and 100 inclusive
+        if(difficultyLevel == 2){
+            return rng.nextInt(101);
+        }
+        //if difficulty is 3, return random int between 1 and 1000 inclusive
+        if(difficultyLevel == 3){
+            return rng.nextInt(1001);
+        }
+        System.out.println("Invalid difficulty");
+        return -1;
+    }
+
+    private void nextGuessMessage(int numberOfGuesses, String responseMessage){
+        //if numberOfGuesses is zero, prints "I have my number. What's your guess?"
+            //else, prints responseMessage + "Guess again:"
+        if(numberOfGuesses == 0){
+            System.out.println("I have my number. What's your guess?");
+        }
+        else{
+            System.out.printf("%s Guess again:%n",responseMessage);
+        }
+    }
+
+    public int promptGuess(int numberOfGuesses, String responseMessage){
+
+        //Call a method that prints the appropriate prompt message:
+        nextGuessMessage(numberOfGuesses, responseMessage);
+
+        //tries to return the entered value as an integer
+        //if NFE, return -1
+        try{
+            return Integer.parseInt(userInput.nextLine());
+        }catch(NumberFormatException nfe){
+            return -1;
+        }
+    }
+
+    private boolean checkIfCorrect(int currentGuess, int targetNumber){
+        return(currentGuess == targetNumber);
+    }
+
+    public String compareGuess(int currentGuess, int targetNumber){
+        //compares entered value to target number
+            //if too high, return "Too high."
+            //if too low, return "Too low."
+            //if equal, return "X" (something went wrong)
+        if(currentGuess > targetNumber){
+            return "Too high.";
+        }
+        else if(currentGuess < targetNumber){
+            return "Too low.";
+        }
+        else{
+            return "X";
+        }
+    }
+
+    private boolean rightAnswer(int numberOfGuesses){
+        System.out.printf("You got it in %d guesses!%n%nDo you wish to play again (Y/N)?%n", numberOfGuesses);
+
+        return userInput.nextLine().equals("Y");
+    }
 
     public static void main(String[] args) {
         //print "Let's play Guess the Number!"
-        
+        System.out.println("Let's play Guess the Number!");
+
+        int difficultyLevel;
+        int currentGuess;
+        int numberOfGuesses = 0;
+        boolean programExit = false;
+        String responseMessage = "";
+        Solution32 solutionApp = new Solution32();
+
         //in a loop:
-            //Call a function that:
-                //in a (second) loop:
-                    //prompts player for difficulty level
-                    //tries to parse that value as an int
-                        //If NFE, prints error message (must enter an int 1, 2, or 3) and restart loop
-                    //checks if the value is between 1 and 3 inclusive
-                        //If it is, returns the entered difficulty level
-                    //prints error message
+        do {
+            //Call a method that prompts difficulty:
+            difficultyLevel = solutionApp.promptDifficulty();
 
-            //Call a function using difficulty level as parameter that:
-                //if difficulty is 1, sets random target between 1 and 10 inclusive
-                //if difficulty is 2, sets random target between 1 and 100 inclusive
-                //if difficulty is 3, sets random target between 1 and 1000 inclusive
-                //returns the random target
+            //Call a method that generates a random target number:
+            int targetNumber = solutionApp.randomTarget(difficultyLevel);
+            if (targetNumber == -1) {
+                System.out.println("Invalid difficulty, exiting");
+                System.exit(1);
+            }
 
-            //Call a function using random target as parameter that:
-            //sets an integer numberOfGuesses and a String responseMessage
-                //in a (second) loop:
-                    //calls a helper method that:
-                        //if numberOfGuesses is zero, prints "I have a number. What's your guess?"
-                            //else, prints responseMessage + "Guess again:"
-                        //tries to parse the entered value as a number
-                            //if NFE, set responseMessage to "Not a number."
-                        //returns the number
-                    //sets responseMessage using a helper method using target number and entered number as parameter that:
-                        //compares entered value to target number
-                            //if too high, return "Too high."
-                            //if too low, return "Too low."
-                            //if equal, return "equal"
-                    //if returned "equal", call a helper method using numberOfGuesses as parameter that:
-                        //prints "You got it in (numberOfGuesses) guesses!/n/nDo you wish to play again (Y/N)?"
-                            //if entered string is Y, return true
-                            //else, exit program
-                    //if returned true, break loop
-                    //end/restart (second) loop
+            while (true) {
+                //Call a method that prompts the user for a guess and returns it:
+                currentGuess = solutionApp.promptGuess(numberOfGuesses, responseMessage);
+                //Set number of guesses +1
+                numberOfGuesses++;
+
+                //Call a method that checks if the guess is correct:
+                if (solutionApp.checkIfCorrect(currentGuess, targetNumber)) {
+                    //If it's correct, call a method to congratulate and ask if they want to play again
+                    if (solutionApp.rightAnswer(numberOfGuesses)) {
+                        //If they do, reset number of guesses
+                        numberOfGuesses = 0;
+                    } else {
+                        //If they don't, exit the program
+                        programExit = true;
+                    }
+                    //Break this loop to start a new game
+                    break;
+                }
+
+                //Call a method that compares the guess to the target and determines the response message:
+                responseMessage = solutionApp.compareGuess(currentGuess, targetNumber);
+                //If response message returns "X", something went wrong
+                if(responseMessage.equals("X")){
+                    System.exit(1);
+                }
+            }
+        } while (!programExit);
     }
 }
--- exercise32/src/test/java/Solution32Test.java
+++ exercise32/src/test/java/Solution32Test.java
@@ -1,4 +1,28 @@
+import baseline.Solution32;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class Solution32Test {
-  
+
+    @Test
+    void compareGuess_test() {
+        Solution32 testApp = new Solution32();
+
+        String expected = "X";
+        String actual = testApp.compareGuess(5, 5);
+        assertEquals(expected, actual);
+
+        String expected2 = "Too high.";
+        String actual2 = testApp.compareGuess(10, 5);
+        assertEquals(expected2, actual2);
+    }
+
+    @Test
+    void randomTarget_test(){
+        Solution32 testApp = new Solution32();
+
+        int actual = testApp.randomTarget(1);
+        assertTrue(actual >= 1 && actual <= 10);
+    }
 }
