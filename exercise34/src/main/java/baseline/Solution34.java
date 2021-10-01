23e1aeee7c2adf8ec052d77b908daaef2d43e10d
unknown <detrich.lange@gmail.com>
2021-10-01 15:47:19 -0400

    pseudocode for 34

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,28 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Detrich Lange
+ */
+
+package baseline;
+import java.util.Scanner;
+
+/*
+Create a small program that contains a list of employee names. Print out the list of names when the program runs the
+first time. Prompt for an employee name and remove that specific name from the list of names. Display the remaining
+employees, each on its own line.
+    Use an array or list to store the names.
+ */
+
+public class Solution34 {
+    static final Scanner userInput = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        //Create array with the five names
+        //Print the employees in the array
+        //Prompt user to enter a name
+        //Create a new array
+        //For each entry in the first array, if the string does not match the user inputted string, copy to the second array
+        //Print the employees in the second array
+    }
+}
