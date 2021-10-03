a1a70fb6cd1c8c1aca1bcc8bb67088fcbc3e6226
unknown <detrich.lange@gmail.com>
2021-10-03 15:55:51 -0400

    pseudocode for 40

---
+++ exercise40/src/main/java/baseline/Solution40.java
@@ -0,0 +1,28 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Detrich Lange
+ */
+
+package baseline;
+import java.util.*;
+
+/*
+Given the following data set create a program that lets a user locate all records that match the search string by
+comparing the search string to the first or last name field.
+    Store the data using a list of maps.
+ */
+
+public class Solution40 {
+
+    //Make the list of maps containing the employee information using the code I already wrote for 39
+
+    //Prompt the user to enter a search string
+
+    //Print table header
+
+    //For each map in the list:
+        //Check if the first name contains the search string:
+            //If it does, print the employee's data.
+            //If it doesn't, check if the last name contains the search string:
+                //If it does, print the employee's data.
+}
