1e598f2ed05dda6b5d591dc380d8b4699a14bb51
unknown <detrich.lange@gmail.com>
2021-10-03 14:15:59 -0400

    pseudocode for 39

---
+++ exercise39/src/main/java/baseline/Solution39.java
@@ -0,0 +1,27 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Detrich Lange
+ */
+
+package baseline;
+import java.util.Scanner;
+
+/*
+Given the following data set, create a program that sorts all employees by last name and prints them to the screen in a
+tabular format.
+    Store the data using a list of maps.
+ */
+
+public class Solution39 {
+    static final Scanner userInput = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Solution39 solutionApp = new Solution39();
+
+        //Create a list of maps
+        //Add a map for each employee
+        //Sort the list alphabetically
+        //Print the table header
+        //Print each map's contents alphabetically by last name
+    }
+}
