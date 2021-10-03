9a0d6009f40a4676315d4b903c36dcaf0760c28e
unknown <detrich.lange@gmail.com>
2021-10-03 11:39:13 -0400

    pseudocode for 38

---
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,34 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Detrich Lange
+ */
+
+package baseline;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+/*
+Create a program that prompts for a list of numbers, separated by spaces. Have the program print out a new list
+containing only the even numbers.
+    Convert the input to an array. Many languages can easily convert strings to arrays with a built-in function that
+    splits apart a string based on a specified delimiter.
+    Write your own algorithm—don’t rely on the language’s built-in filter or similar enumeration feature.
+    Use a function called filterEvenNumbers to encapsulate the logic for this. The function takes in the original array
+    and returns the new array.
+ */
+
+public class Solution38 {
+    static final Scanner userInput = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //Prompt the user for a string
+
+        //Create an array by splitting the string at each space
+
+        //For each item in the array:
+            //If the number modulo 2 is 0, print it
+            //If NFE, print error message
+    }
+}
